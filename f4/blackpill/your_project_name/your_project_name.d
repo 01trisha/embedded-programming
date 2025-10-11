8338b171640b3def8fb84557c77c4f1c06d8c697
01trisha <daniltrishkin5@gmail.com>
2025-10-12 05:01:25 +0700

    смог мигать каждые несколько секунд рабочее

---
+++ f4/blackpill/your_project_name/your_project_name.d
@@ -0,0 +1,24 @@
+your_project_name.o: your_project_name.c your_project_name.h \
+ ../../../libopencm3//include/libopencm3/stm32/gpio.h \
+ ../../../libopencm3//include/libopencm3/cm3/common.h \
+ /opt/gcc-arm/lib/gcc/arm-none-eabi/14.3.1/include/stdint.h \
+ /opt/gcc-arm/arm-none-eabi/include/stdint.h \
+ /opt/gcc-arm/arm-none-eabi/include/machine/_default_types.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/features.h \
+ /opt/gcc-arm/arm-none-eabi/include/_newlib_version.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/_intsup.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/_stdint.h \
+ /opt/gcc-arm/lib/gcc/arm-none-eabi/14.3.1/include/stdbool.h \
+ ../../../libopencm3//include/libopencm3/stm32/memorymap.h \
+ ../../../libopencm3//include/libopencm3/stm32/f4/memorymap.h \
+ ../../../libopencm3//include/libopencm3/cm3/memorymap.h \
+ ../../../libopencm3//include/libopencm3/stm32/f4/gpio.h \
+ ../../../libopencm3//include/libopencm3/stm32/common/gpio_common_f24.h \
+ ../../../libopencm3//include/libopencm3/stm32/common/gpio_common_f234.h \
+ ../../../libopencm3//include/libopencm3/stm32/common/gpio_common_all.h \
+ ../../../libopencm3//include/libopencm3/stm32/rcc.h \
+ ../../../libopencm3//include/libopencm3/stm32/f4/rcc.h \
+ ../../../libopencm3//include/libopencm3/stm32/pwr.h \
+ ../../../libopencm3//include/libopencm3/stm32/f4/pwr.h \
+ ../../../libopencm3//include/libopencm3/stm32/common/pwr_common_v1.h \
+ ../../../libopencm3//include/libopencm3/stm32/common/rcc_common_all.h
---
+++ f4/blackpill/your_project_name/your_project_name.h
@@ -0,0 +1,6 @@
+// Содержимое your_project_name.h
+#ifndef YOUR_PROJECT_NAME_H
+#define YOUR_PROJECT_NAME_H
+
+// Объявления функций, если они нужны
+#endif
